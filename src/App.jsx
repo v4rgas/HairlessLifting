10ee70cc2ac319aacea29557c405e0c4c0f8ccb0
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-07-08 19:59:57 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material'
+
+function App() {
+  return (
+    <Box>
+      <Typography variant="h1">Hello, world!</Typography>
+    </Box>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import App from './App.jsx'
+import { CssBaseline } from '@mui/material';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CssBaseline />
+    <App />
+  </React.StrictMode>,
+)
