28ed8f7e7a6632722e4bb7cd266b16c9766c3a73
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-07-11 23:39:11 -0400

    refactor: Remove unused code and update SplitTracker component

--- src/SplitTracker/SplitTracker.jsx
+++ src/SplitTracker/SplitTracker.jsx
@@ -1,23 +1,14 @@
-import { Container, TableContainer, Table, TableHead, TableRow, TableCell, Paper, Typography, Divider, TableBody  } from "@mui/material";
+import { Container } from "@mui/material";
 import { useParams } from "react-router-dom";
 import useStorage from "../utils/useStorage";
 import GoBackButton from "../GoBackButton";
 
 export default function SplitTracker() {
-    const { splitId } = useParams();
-
-    const { getSplits } = useStorage();
-    const split = getSplits()[splitId];
-
-
-    
 
 
     return (
         <Container>
-            <pre>{JSON.stringify(split, null, 2)}</pre>
 
- 
             <GoBackButton/>
         </Container>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
         element: <SplitViewer/>
       },
       {
-        path: "/tracker/:splitId",
+        path: "/tracker",
         element: <SplitTracker/>
       },
       
