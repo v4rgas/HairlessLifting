dacc0bfd44b7da36a23d9a4b36c85a6cdc5dbb3f
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-07-12 00:16:45 -0400

    feat: Improve SavedSplits page with lazy loading for split buttons and add GoBackButton component

--- src/SavedSplits/SavedSplits.jsx
+++ src/SavedSplits/SavedSplits.jsx
@@ -2,6 +2,7 @@ import { Button, Container, Stack, Typography } from "@mui/material"
 import useStorage from "../utils/useStorage"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import GoBackButton from "../GoBackButton"
 
 export default function SavedSplits(){
     const { getSplits } = useStorage()
@@ -18,7 +19,13 @@ export default function SavedSplits(){
                 <Typography variant="h3">
                     Saved splits
                 </Typography>
-                {Object.values(splits).map((split)=><Button onClick={()=>navigate("/split/"+split.id)} variant="contained">{split.splitName}</Button>) }
+                {Object.values(splits).map((split)=>
+                <Button onClick={()=>navigate("/split/"+split.id)} variant="contained">
+                <Typography variant="h4">
+                    {split.splitName}
+                </Typography>
+                </Button>) }
+                <GoBackButton />
             </Stack>
         </Container>
        
