008a7893bada66874ae13102a6af58f471d86d74
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-08-02 10:27:53 -0400

    chore: Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pelao-rqlo',
   plugins: [react(),
     VitePWA({ 
       registerType: 'autoUpdate',
