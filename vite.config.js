0e6a1d4bbe54ccda758a54b4017ebfefecfc0826
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-08-02 10:45:49 -0400

    build: Update rollupOptions in vite.config.js for main and 404 pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        404: '404.html'
+      }
+    }
+  },
   base: '/pelao-rqlo',
   plugins: [react(),
     VitePWA({ 
